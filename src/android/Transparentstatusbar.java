ba972f852b75c867305ddd5d2b40255f695ddfcd
manugando <emanuele.gandolfi89@gmail.com>
2017-02-18 14:22:28 +0100

    Manage success callback

--- src/android/Transparentstatusbar.java
+++ src/android/Transparentstatusbar.java
@@ -9,6 +9,7 @@ public class Transparentstatusbar extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
        if (action.equals("customaction")) {
             String message = args.getString(0);
+            callbackContext.success("YES! " . message);
             Log.i("Gando", "Plugin called! " . message);
             return true;
         } else {
